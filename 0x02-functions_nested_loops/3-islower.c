0156b5ab9fa3eb495d18d6545ccc14940395423c
fsam97 <tbam1997@gmail.com>
2022-09-13 13:55:17 -0700

    My first commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * _islower - Shows 1 if the input is a lowercase chaeacter. Another cases, shows 0
+ * _islower - Shows 1 if the input is a
+ * lowercase chaeacter. Another cases, shows 0
+ *
  * @c: The character in ASCII code
  *
  * Return: 1 for lowercase. 0 for tge rest.
