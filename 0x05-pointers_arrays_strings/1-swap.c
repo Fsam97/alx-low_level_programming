d53179e42d74b4b3c1907790de3c444d9eef9f0d
fsam97 <tbam1997@gmail.com>
2022-09-19 08:14:30 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - tajes two integer variable and swap them
+ * @a: first variable
+ * @b: second variable
+ *
+ * created by: Maconi Fiyunfoluwa Samuel
+ * cc: 19 september, 2022
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
